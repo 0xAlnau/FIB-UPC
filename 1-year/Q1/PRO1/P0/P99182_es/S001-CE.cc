072993cb2c564ce35bceb9105df2546a18edf815
0xAlnau <cullellarnau@gmail.com>
2025-03-24 22:11:50 +0100

    Add files via upload

---
+++ 1-year/Q1/PRO1/P0/P99182_es/S001-CE.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    int a, b;
+    cin >> a >> b;
+    int c=(a+b)/2.0;
+    cout << "c";
+
+}
---
+++ 1-year/Q1/PRO1/P0/P99182_es/S002-AC.cc
@@ -0,0 +1,12 @@
+//este programa calculará el promedio entre dos numeros naturales
+//input: dos numeros enteros entre el -1000 i el 1000
+//output: el promedio de los dos numeros
+#include <iostream>
+using namespace std;
+int main () {
+    int a, b;
+    cin >> a >> b;
+    double c=(a + b) / 2.0;
+    cout << c <<endl;
+
+}
