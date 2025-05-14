a4d817d49983825204960b0e946e2a63d71aa73b
0xAlnau <cullellarnau@gmail.com>
2025-05-14 16:17:28 +0200

    Add files via upload

---
+++ 2-year/Q2/IDI/ExercicisLab3/L3.1/E5/MyGLWidget.cpp
@@ -0,0 +1,243 @@
+// MyGLWidget.cpp
+#include "MyGLWidget.h"
+#include <iostream>
+#include <stdio.h>
+
+#define printOpenGLError() printOglError(__FILE__, __LINE__)
+#define CHECK() printOglError(__FILE__, __LINE__,__FUNCTION__)
+#define DEBUG() std::cout << __FILE__ << " " << __LINE__ << " " << __FUNCTION__ << std::endl;
+
+void MyGLWidget::initializeGL ()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  initializeOpenGLFunctions();
+
+  glClearColor(0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+  glEnable(GL_DEPTH_TEST);
+
+  carregaShaders();
+  creaBuffersPatricio();
+  creaBuffersTerraIParet();
+
+  iniEscena();
+  iniCamera();
+}
+
+int MyGLWidget::printOglError(const char file[], int line, const char func[]) 
+{
+    GLenum glErr;
+    int    retCode = 0;
+
+    glErr = glGetError();
+    const char * error = 0;
+    switch (glErr)
+    {
+        case 0x0500:
+            error = "GL_INVALID_ENUM";
+            break;
+        case 0x501:
+            error = "GL_INVALID_VALUE";
+            break;
+        case 0x502: 
+            error = "GL_INVALID_OPERATION";
+            break;
+        case 0x503:
+            error = "GL_STACK_OVERFLOW";
+            break;
+        case 0x504:
+            error = "GL_STACK_UNDERFLOW";
+            break;
+        case 0x505:
+            error = "GL_OUT_OF_MEMORY";
+            break;
+        default:
+            error = "unknown error!";
+    }
+    if (glErr != GL_NO_ERROR)
+    {
+        printf("glError in file %s @ line %d: %s function: %s\n",
+                             file, line, error, func);
+        retCode = 1;
+    }
+    return retCode;
+}
+
+MyGLWidget::~MyGLWidget() {
+}
+
+void MyGLWidget::mouseMoveEvent(QMouseEvent *e)
+{
+  makeCurrent();
+  // Aqui cal que es calculi i s'apliqui la rotacio o el zoom com s'escaigui...
+  if (DoingInteractive == ROTATE)
+  {
+    // Fem la rotació
+    angleY += (e->x() - xClick) * M_PI / ample;
+    viewTransform ();
+  }
+
+  xClick = e->x();
+  yClick = e->y();
+
+  update ();
+}
+
+void MyGLWidget::keyPressEvent(QKeyEvent* event) {
+  makeCurrent();
+  switch (event->key()) {
+  /*case : 
+      ...
+      break;
+  */  
+    default: BL3GLWidget::keyPressEvent(event); break;
+  }
+  update();
+}
+
+void MyGLWidget::iniMaterialTerra ()
+{
+  // Donem valors al material del terra
+  amb = glm::vec3(0.2,0,0.2);
+  diff = glm::vec3(0,0,1); //color!!
+  spec = glm::vec3(0,0,0);
+  shin = 100000; //brillantor (plastic)
+}
+
+void MyGLWidget::creaBuffersTerraIParet ()
+{
+  // VBO amb la posició dels vèrtexs
+  glm::vec3 posterra[12] = {
+	glm::vec3(-1.0, -1.0, 1.0),
+	glm::vec3(1.0, -1.0, 1.0),
+	glm::vec3(-1.0, -1.0, -1.0),
+	glm::vec3(-1.0, -1.0, -1.0),
+	glm::vec3(1.0, -1.0, 1.0),
+	glm::vec3(1.0, -1.0, -1.0),
+	glm::vec3(-1.0, -1.0, -1.0),
+	glm::vec3(1.0, -1.0, -1.0),
+	glm::vec3(-1.0, 1.0, -1.0),
+	glm::vec3(-1.0, 1.0, -1.0),
+	glm::vec3(1.0, -1.0, -1.0),
+	glm::vec3(1.0, 1.0, -1.0)
+  };
+
+  // VBO amb la normal de cada vèrtex
+  glm::vec3 norm1 (0,1,0);
+  glm::vec3 norm2 (0,0,1);
+  glm::vec3 normterra[12] = {
+	norm1, norm1, norm1, norm1, norm1, norm1, // la normal (0,1,0) per als primers dos triangles
+	norm2, norm2, norm2, norm2, norm2, norm2  // la normal (0,0,1) per als dos últims triangles
+  };
+
+  iniMaterialTerra();
+
+  // Fem que el material del terra afecti a tots els vèrtexs per igual
+  glm::vec3 matambterra[12] = {
+	amb, amb, amb, amb, amb, amb, amb, amb, amb, amb, amb, amb
+  };
+  glm::vec3 matdiffterra[12] = {
+	diff, diff, diff, diff, diff, diff, diff, diff, diff, diff, diff, diff
+  };
+  glm::vec3 matspecterra[12] = {
+	spec, spec, spec, spec, spec, spec, spec, spec, spec, spec, spec, spec
+  };
+  float matshinterra[12] = {
+	shin, shin, shin, shin, shin, shin, shin, shin, shin, shin, shin, shin
+  };
+
+// Creació del Vertex Array Object del terra
+  glGenVertexArrays(1, &VAO_Terra);
+  glBindVertexArray(VAO_Terra);
+
+  GLuint VBO_Terra[6];
+  glGenBuffers(6, VBO_Terra);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra[0]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(posterra), posterra, GL_STATIC_DRAW);
+
+  // Activem l'atribut vertexLoc
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertexLoc);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra[1]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(normterra), normterra, GL_STATIC_DRAW);
+
+  // Activem l'atribut normalLoc
+  glVertexAttribPointer(normalLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(normalLoc);
+
+  // En lloc del color, ara passem tots els paràmetres dels materials
+  // Buffer de component ambient
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra[2]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(matambterra), matambterra, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(matambLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(matambLoc);
+
+  // Buffer de component difusa
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra[3]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(matdiffterra), matdiffterra, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(matdiffLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(matdiffLoc);
+
+  // Buffer de component especular
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra[4]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(matspecterra), matspecterra, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(matspecLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(matspecLoc);
+
+  // Buffer de component shininness
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_Terra[5]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(matshinterra), matshinterra, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(matshinLoc, 1, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(matshinLoc);
+
+  glBindVertexArray(0);
+}
+
+void MyGLWidget::carregaShaders()
+{
+  // Creem els shaders per al fragment shader i el vertex shader
+  QOpenGLShader fs (QOpenGLShader::Fragment, this);
+  QOpenGLShader vs (QOpenGLShader::Vertex, this);
+  // Carreguem el codi dels fitxers i els compilem
+  fs.compileSourceFile("shaders/basicLlumShader.frag");
+  vs.compileSourceFile("shaders/basicLlumShader.vert");
+  // Creem el program
+  program = new QOpenGLShaderProgram(this);
+  // Li afegim els shaders corresponents
+  program->addShader(&fs);
+  program->addShader(&vs);
+  // Linkem el program
+  program->link();
+  // Indiquem que aquest és el program que volem usar
+  program->bind();
+
+  // Obtenim identificador per a l'atribut “vertex” del vertex shader
+  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+  // Obtenim identificador per a l'atribut “normal” del vertex shader
+  normalLoc = glGetAttribLocation (program->programId(), "normal");
+  // Obtenim identificador per a l'atribut “matamb” del vertex shader
+  matambLoc = glGetAttribLocation (program->programId(), "matamb");
+  // Obtenim identificador per a l'atribut “matdiff” del vertex shader
+  matdiffLoc = glGetAttribLocation (program->programId(), "matdiff");
+  // Obtenim identificador per a l'atribut “matspec” del vertex shader
+  matspecLoc = glGetAttribLocation (program->programId(), "matspec");
+  // Obtenim identificador per a l'atribut “matshin” del vertex shader
+  matshinLoc = glGetAttribLocation (program->programId(), "matshin");
+
+  // Demanem identificadors per als uniforms del vertex shader
+  transLoc = glGetUniformLocation (program->programId(), "TG");
+  projLoc = glGetUniformLocation (program->programId(), "proj");
+  viewLoc = glGetUniformLocation (program->programId(), "view");
+  posFocus = glGetUniformLocation (program->programId(), "posFocus");
+  colorFocus = glGetUniformLocation (program->programId(), "colorFocus");
+
+  //uniforms per focus!!!!
+  glm::vec3 a = glm::vec3(0.8, 0.8, 0.8);
+  glm::vec3 b = glm::vec3(1, 0, 1);  // en SCA
+  glUniform3fv(posFocus, 1, &b[0]);
+  glUniform3fv(colorFocus, 1, &a[0]);
+}
